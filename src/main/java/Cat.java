2ad87e9beeaae6e377c0067a4afef9503fac0050
Vestrarose <98038489+Vestrarose@users.noreply.github.com>
2022-05-14 07:10:02 +0300

    PP_2_1_2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String message;
+
+    private String getMessage(){
+        return message;
+        }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+}
